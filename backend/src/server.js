4a3dcc9ef6cfa042119c3a61dd34ef73cb7c714e
Shivam Khandare <shivamsk3019@gmail.com>
2025-07-01 15:07:05 +0530

    Adding Ratelimit & Completing Backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,20 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
 
-connectDB();
-
-app.use(express.json());                                           // This will allow to access the value of title and content to req.body
+app.use(express.json())                                            // This middleware will parse JSON bodies: req.body
+app.use(rateLimiter)
 
 app.use("/api/notes",notesRoutes)                                  // This will prefix the routes of notesRoutes to /api/notes
 
-app.listen(PORT,()=>{
-    console.log("Server is running on port:", PORT)
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Server is running on port:", PORT)
+    })
 })
